eec2ffd85259b42dec02a159c10836b7a5d9335a
jason <jason@jbrmmg.me.uk>
2022-12-08 18:15:16 +0000

    TFL Api testing

---
+++ tfl/src/test/java/com/jbr/exp/tfl/TflApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbr.exp.tfl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TflApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
